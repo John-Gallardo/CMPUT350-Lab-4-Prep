d7a91ce39941eaba66e78630bfbc5bf1a5b8121d
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-09-19 15:47:25 -0600

    feat: add skeleton for timer & aliases for std::chrono time types

--- src/timer.h
+++ src/timer.h
@@ -1,4 +1,35 @@
 #ifndef TIMER_H
 #define TIMER_H
+#include <chrono>
+#include <cstdint>
+
+class Timer {
+    public:
+        // aliases
+        using Nanos   = std::chrono::nanoseconds;
+        using Micros  = std::chrono::microseconds;
+        using Millis  = std::chrono::milliseconds;
+        using Seconds = std::chrono::seconds;
+        using Minutes = std::chrono::minutes;
+        using Hours   = std::chrono::hours;
+
+        // constructor
+        Timer() {
+            // TODO:
+        }
+
+        // resets state of timer
+        void restart() {
+            // TODO:
+        }
+
+        // returns time unit T since timer was last clicked/restarted/constructed
+        template <typename T>
+        uint64_t click() {
+            // TODO:
+        }
+    private:
+
+};
 
 #endif  // TIMER_H
