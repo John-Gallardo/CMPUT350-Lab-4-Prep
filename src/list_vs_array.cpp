28b6237d7a3e7adf05d9a39f4b038e162cca9347
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-09-19 19:38:24 -0600

    feat: add glance() which I forgot & add linked list test. also finish Timer implementation

--- src/list_vs_array.cpp
+++ src/list_vs_array.cpp
@@ -1,10 +1,24 @@
 #include <cstddef>  // size_t
 
 #include "timer.h"
+#include <list>
+#include <vector>
+#include <random> 
+#include <iostream>
 
 constexpr size_t SIZE = 16000000;
 
 int main() {
     // test here...
+    std::mt19937_64 rng(0);
+
+    // 1. Linked list test
+    std::list<uint64_t> linkedList{};
+    Timer listTimer{};
+    for (size_t i{0}; i < SIZE; i++) {
+        linkedList.push_back(rng());
+    }
+    std::cout << "Linked List: " << listTimer.click<Timer::Millis>() << "ms\n";
+
     return 0;
 }
--- src/timer.h
+++ src/timer.h
@@ -14,22 +14,34 @@ class Timer {
         using Hours   = std::chrono::hours;
 
         // constructor
-        Timer() {
-            // TODO:
-        }
+        Timer()
+        : m_time{std::chrono::steady_clock::now()} {}
 
         // resets state of timer
         void restart() {
-            // TODO:
+            m_time = std::chrono::steady_clock::now();
         }
 
         // returns time unit T since timer was last clicked/restarted/constructed
+        // NOTE: keeping track of a separate timer for click/restart/constructed seemed redundant
+        // because we only need to keep the most recent one so there is only m_time.
         template <typename T>
         uint64_t click() {
-            // TODO:
+            auto timeNow{std::chrono::steady_clock::now()};
+            // static cast to stop warning
+            uint64_t timeElapsed{static_cast<uint64_t>(std::chrono::duration_cast<T>(timeNow - m_time).count())};
+            restart();
+            return timeElapsed;
+        }
+
+        // literally the same as click() but doesn't restart time
+        template <typename T>
+        uint64_t glance() {
+            auto timeNow{std::chrono::steady_clock::now()};
+            return std::chrono::duration_cast<T>(timeNow - m_time).count();
         }
     private:
-
+        std::chrono::time_point<std::chrono::steady_clock> m_time{};
 };
 
 #endif  // TIMER_H
