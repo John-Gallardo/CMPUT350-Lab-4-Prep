23dddd510b3f4c8595671a5d0ad59bb51ebdbac1
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-09-19 19:47:24 -0600

    feat: finish testing vector

--- src/list_vs_array.cpp
+++ src/list_vs_array.cpp
@@ -14,11 +14,26 @@ int main() {
 
     // 1. Linked list test
     std::list<uint64_t> linkedList{};
-    Timer listTimer{};
+    Timer timer{};
     for (size_t i{0}; i < SIZE; i++) {
         linkedList.push_back(rng());
     }
-    std::cout << "Linked List: " << listTimer.click<Timer::Millis>() << "ms\n";
+    std::cout << "Linked List: " << timer.click<Timer::Millis>() << "ms\n";
+
+    // 2. Vector test without reserve
+    std::vector<uint64_t> vectorTestNoReserve{};
+    for (size_t i{0}; i < SIZE; i++) {
+        vectorTestNoReserve.push_back(rng());
+    }
+    std::cout << "Vector Test No Reserve: " << timer.click<Timer::Millis>() << "ms\n";
+
+    // 3. Vector test with reserve
+    std::vector<uint64_t> vectorTestWithReserve{};
+    vectorTestWithReserve.reserve(SIZE);
+    for (size_t i{0}; i < SIZE; i++) {
+        vectorTestWithReserve.push_back(rng());
+    }
+    std::cout << "Vector Test With Reserve: " << timer.click<Timer::Millis>() << "ms\n";
 
     return 0;
 }
